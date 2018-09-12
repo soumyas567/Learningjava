f1e5d295ecef5613b9d61a576ee10fa1f4972d9c
soumya somasundaran <soumyas567@gmail.com>
2018-09-12 11:23:20 +0530

    forloop:enhanced

---
+++ Enhancedloopexample.java
@@ -0,0 +1,20 @@
+/*
+
+Enhanced for loop is used when you have an array or a collection. The counter variable will iterate through the collection
+*/
+
+class Enhancedloopexample {
+   public static void main(String args[]){
+      int arr[]={1,2,3,4};
+      String array[]={"soumya","atiqah","praveen"};
+
+      for (int num : arr) {
+         System.out.println(num);
+      }
+   for(String s:array)
+   {
+     //Data type of i is String
+     System.out.println(s);
+   }
+}
+}
